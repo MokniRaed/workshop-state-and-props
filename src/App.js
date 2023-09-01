9ffe84004d48b604c2390662f8c427c61a00d859
raedmokni <raedmokni2@gmail.com>
2023-09-01 23:23:51 +0100

    fC

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Button from "react-bootstrap/Button";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
 function App() {
+  const [todos, setTodos] = useState(["Check the pizza ", "go to Ahmed house"]);
+  const [newTodo, setNewTodo] = useState("");
+
+  const handleKeyPress = (e) => {
+    if (e.keyCode === 13) {
+      handleAddTodo();
+    }
+  };
+  const handleAddTodo = () => {
+    if (newTodo !== "") {
+      setTodos([...todos, newTodo]);
+      setNewTodo("");
+    } else {
+      alert("Try to type something Bro 🦕");
+    }
+  };
+  const removeTodo = (index) => {
+    const newTodoList = [...todos];
+    newTodoList.splice(index, 1);
+    setTodos(newTodoList);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <Row className="justify-content-md-center">
+          <h1> My Todos</h1>
+          <input
+            value={newTodo}
+            onKeyDown={(e) => handleKeyPress(e)}
+            onChange={(e) => setNewTodo(e.target.value)}
+            placeholder="Add your new todo here.."
+          ></input>
+          <Button onClick={handleAddTodo} variant="primary">
+            Add
+          </Button>
+          <ol>
+            {todos.map((todo, index) => (
+              <>
+                <li key={index}>
+                  {todo}{" "}
+                  <Button variant="danger" onClick={() => removeTodo(index)}>
+                    X
+                  </Button>
+                </li>
+              </>
+            ))}
+          </ol>
+        </Row>
+      </Container>
     </div>
   );
 }
