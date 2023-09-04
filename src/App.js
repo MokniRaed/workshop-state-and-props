d851db9f27de2c948e69283eff364bb85b63b61c
raedmokni <raedmokni2@gmail.com>
2023-09-04 19:56:40 +0100

    adding comments

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import { useState } from "react";
 import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
 import Button from "react-bootstrap/Button";
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Table from "react-bootstrap/Table";
+
 function App() {
   const [todos, setTodos] = useState(["Check the pizza ", "go to Ahmed house"]);
   const [newTodo, setNewTodo] = useState("");
+  const [editingIndex, setEditingIndex] = useState(null);
 
+  // adding Todo by clicking on  the enter button in the keyboard
   const handleKeyPress = (e) => {
     if (e.keyCode === 13) {
       handleAddTodo();
     }
   };
+
+  // the function to add Todo to the list  of Todos
   const handleAddTodo = () => {
     if (newTodo !== "") {
       setTodos([...todos, newTodo]);
@@ -21,11 +26,19 @@ function App() {
       alert("Try to type something Bro 🦕");
     }
   };
+
+  // Remove todo from the list by it index
   const removeTodo = (index) => {
     const newTodoList = [...todos];
     newTodoList.splice(index, 1);
     setTodos(newTodoList);
   };
+
+  //Additional Function for editing
+  const handleEditClick = (index) => {
+    setEditingIndex(index);
+  };
+
   return (
     <div className="App">
       <Container>
@@ -40,18 +53,22 @@ function App() {
           <Button onClick={handleAddTodo} variant="primary">
             Add
           </Button>
-          <ol>
+          <Table striped borderless hover>
             {todos.map((todo, index) => (
-              <>
-                <li key={index}>
-                  {todo}{" "}
-                  <Button variant="danger" onClick={() => removeTodo(index)}>
-                    X
-                  </Button>
-                </li>
-              </>
+              <tr>
+                <>
+                  {" "}
+                  <td onClick={() => handleEditClick(index)}>{todo}</td>
+                  <td>
+                    {" "}
+                    <Button variant="danger" onClick={() => removeTodo(index)}>
+                      X
+                    </Button>
+                  </td>
+                </>
+              </tr>
             ))}
-          </ol>
+          </Table>
         </Row>
       </Container>
     </div>
